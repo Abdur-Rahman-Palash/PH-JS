8a96e37be1f18e3666a3bc99b6f5f855d55f956f
palash0111 <aymanislammayan@gmail.com>
2024-08-30 15:50:13 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,112 @@
+// 1.
+let burger = 500;
+if(burger < 600 ){
+    console.log("free coke")
+}
+else{
+    console.log("30tk")
+}
+// 2.
+let weight = 80;
+let height = 2;
+let BMI =  weight / (height * height);
+if( BMI < 18.5){
+    console.log(" you are underweight.")
+}
+else if(BMI >= 18.5 && BMI <= 24.9){
+console.log("you are normal.")
+}
+else if(BMI >=25 && BMI <= 29.9){
+    console.log("you are overweight.")
+}
+else{
+    console.log("you are obese.")
+}
+
+// 3.
+let score = 65;
+if(score >=90 && score <=100){
+    console.log("you got 'A'")
+}
+else if(score >=80 && score <=89){
+    console.log("you got 'B'")
+}
+else if(score >=70 && score <=79){
+    console.log("you got 'B'")
+}
+else if(score >=60 && score <=69){
+    console.log("you got 'B'")
+}
+else if(score >=50 && score <=59){
+    console.log("you got 'Fail'")
+}
+else{
+    console.log("tomar jibon bekar")
+}
+// 4.
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+let myScore = 80;
+let friendsScore = 75;
+
+if(friendsScore > myScore){
+    console.log("go for a lunch")
+    if(friendsScore <= 80 && friendsScore >=60 ){
+        console.log("good luck next time")
+    }
+    else if(friendsScore <= 60 && friendsScore >= 40){
+console.log("friend's message unseen.")
+    }
+    else{
+        console.log("block your friend")
+    }
+}
+else{
+    console.log("go to home and sleep and act sad")
+}
+// 5.
+
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+let num1 = 60;
+let num2 =50;
+let result = num1 + num2;
+if(num1 > num2){
+    result = num1*2
+    console.log(result);
+}
+else{
+    console.log(result);
+}
+let num3 = 70;
+let num4 =60;
+let totals = (num3 > num4) ?  num3*2 : num3 + num4 ;
+console.log(totals);
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
---
+++ script.js
@@ -0,0 +1,34 @@
+
+var apple = 200;
+var orange = 300;
+var shopKeeper=800;
+var total= apple + orange;
+var rest= shopKeeper - total;
+console.log(rest);
+
+
+var bangla =30;
+var english =40;
+var math =50;
+var biology =60;
+var humanity =70;
+var total = bangla + english + math + biology + humanity;
+var average = total / 5;
+console.log(average);
+
+var num1 = 32;
+var num2 = 5;
+var result = num1 % num2;
+console.log(result);
+
+var a = isNaN(22)
+console.log(a)
+var num_1 = 0.1;
+var num_2 = 0.2;
+var total = num_1 + num_2;
+console.log(total.toFixed(2));  
+ var num_3 = parseInt("22.4");
+ console.log(num_3);
+ var num_4 = parseFloat("22.5");
+ console.log(num_4);
+//  28 num line ke kivave number e convert korbo
