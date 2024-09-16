b6c3af9581d2cad50161d52dfe6a1702bb3e827a
palash0111 <aymanislammayan@gmail.com>
2024-09-16 23:02:10 +0600

    another part done

---
+++ event handler/event.js
@@ -0,0 +1,28 @@
+// part01
+const makeOrange = document.getElementById("orange");
+makeOrange.onclick = function orange(){
+    document.body.style.backgroundColor= 'orange';
+};
+// part02
+const pink = document.getElementById('pink').addEventListener('click' , function(){
+    document.body.style.backgroundColor = 'pink';
+})
+// part03
+document.getElementById('another').addEventListener('click', function(){
+const handleStatus = document.getElementById('another-text');
+handleStatus.innerText='another part has changed';
+})
+// part04
+document.getElementById('post').addEventListener('click', function(){
+const commentBox = document.getElementById('new-comment');
+const newComment = commentBox.value;
+const content = document.getElementById('content');
+const p = document.createElement('p');
+p.innerText = newComment;
+content.appendChild(p);
+commentBox.value = '';
+})
+// part05
+document.getElementById('btn-delete').addEventListener('keyup', function(event){
+    const text = event.target.value;
+})
